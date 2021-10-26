3c00b3ed7c5a63f40d0c86a5e16002ca6f86cf51
MIkaela Braga <bragamikaela@gmail.com>
2021-10-26 02:26:57 -0300

    desafio 2 completo

---
+++ challenges/desafio2.js
@@ -0,0 +1,15 @@
+db.movies.aggregate([
+  {
+    $match: {
+      $and: [
+        { "imdb.rating": { $gte: 7 } },
+        { $nor: [{ genres: "Crime" }, { genres: "Horror" }] },
+        { $or: [{ rated: "PG" }, { rated: "G" }] },
+        { languages: { $in: ["English", "Spanish"] } }] } },
+  { $project: {
+    _id: false,
+    titulo: "$title",
+    avaliado: "$rated",
+    notaIMDB: "$imdb.rating",
+    votosIMDB: "$imdb.votes",
+    ano: "$year" } }]);
